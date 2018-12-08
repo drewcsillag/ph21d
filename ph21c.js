75f619af83a3eb256c82c0d8a88c42900c953d07
Drew Csillag <drewc@dropbox.com>
2018-12-08 12:16:55 +0000

    more, including ugly ui with f and g labels

--- ph21c.js
+++ ph21c.js
@@ -7,6 +7,11 @@ let g = 0;
 let f = 0;
 let wasresult = 0;
 let registers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let N = 0;
+let I = 0;
+let PV = 0;
+let PMT = 0;
+let FV = 0;
 
 let wasrcl = 0;
 let wassto = 0;
@@ -33,26 +38,29 @@ function buttonSTO() {
   wassto = 1;
   wasrcl = 0;
 }
-function number(n) {
-  if (wasresult) {
-    if (wasresult === 1) {
-      y = x;
-    }
-    wasresult = 0;
 
-    x = 0;
-  }
+function number(n) {
   if (wasrcl) {
     x = registers[n];
     showx();
     wasrcl = 0;
+    wasresult = 1;
     return;
   }
   if (wassto) {
     registers[n] = x;
     wassto = 0;
+    wasresult = 1;
     showx();
     return;
+  }
+  if (wasresult) {
+    if (wasresult === 1) {
+      y = x;
+    }
+    wasresult = 0;
+    dec = 0;
+    x = 0;
   }
   if (dec === 0) {
     x = x * 10 + n;
@@ -62,12 +70,14 @@ function number(n) {
   }
   showx();
 }
+
 function buttonG() {
   f = 0;
   wassto = 0;
   wasrcl = 0;
   g = 1;
 }
+
 function button0() {
   if (g) {
     // mean
@@ -81,15 +91,75 @@ function button0() {
   number(0);
 }
 
+function computeABr() {
+  let sy = registers[4];
+  let sy2 = registers[5];
+  let n = registers[1];
+  let sx = registers[2];
+  let sxy = registers[6];
+  let sx2 = registers[3];
+  let Bnum = sxy - (sx * sy) / n;
+  let Bden = sx2 - (sx * sx) / n;
+  let B = Bnum / Bden;
+  let A = sy / n - B * (sx / n);
+
+  let Rnum = sxy - (sx * sy) / n;
+  let Rden1 = sx2 - (sx * sx) / n;
+  let Rden2 = sy2 - (sy * sy) / n;
+  let R = Rnum / (Rden1 * Rden2) ** 0.5;
+  return [A, B, R];
+}
+
 function button1() {
+  if (g) {
+    // xhat, r
+    let ab = computeABr();
+    let A = ab[0];
+    let B = ab[1];
+    let R = ab[2];
+    x = (x - A) / B;
+    y = R;
+    wasresult = 1;
+    g = 0;
+    showx();
+    return;
+  }
   number(1);
 }
 
 function button2() {
+  if (g) {
+    // yhat,r
+    let ab = computeABr();
+    let A = ab[0];
+    let B = ab[1];
+    let R = ab[2];
+    x = A + B * x;
+    y = R;
+    wasresult = 1;
+    g = 0;
+    showx();
+    return;
+  }
   number(2);
 }
 
+function afterGUnary() {
+  g = 0;
+  wasresult = 1;
+  showx();
+}
 function button3() {
+  if (g) {
+    // factorial
+    let c = x;
+    while (c > 1) {
+      c -= 1;
+      x *= c;
+    }
+    afterGUnary();
+    return;
+  }
   number(3);
 }
 
@@ -101,6 +171,13 @@ function button5() {
   number(5);
 }
 function button6() {
+  if (g) {
+    x = registers[6] / registers[2];
+    g = 0;
+    showx();
+    wasresult = 1;
+    return;
+  }
   number(6);
 }
 function button7() {
@@ -137,8 +214,35 @@ function buttonPoint() {
   showx();
 }
 
+function buttonSingleStep() {
+  if (f) {
+    // clear stats
+    for (let i = 1; i < 7; i++) {
+      registers[i] = 0;
+    }
+    f = 0;
+    return;
+  }
+  alert('notimplemented');
+}
 function buttonCLx() {
+  if (f) {
+    for (let i = 0; i < 10; i++) {
+      registers[i] = 0;
+    }
+    f = 0;
+    stack3 = 0;
+    stack4 = 0;
+    y = 0;
+    N = 0;
+    I = 0;
+    PMT = 0;
+    PV = 0;
+    FV = 0;
+  }
+
   x = 0;
+  dec = 0;
   showx();
 }
 
@@ -166,6 +270,12 @@ function buttonMinus() {
   afterBinaryOp();
 }
 function buttonTimes() {
+  if (g) {
+    // square
+    x = x * x;
+    afterGUnary();
+    return;
+  }
   x = y * x;
   afterBinaryOp();
 }
@@ -180,19 +290,54 @@ function buttonCHS() {
   showx();
 }
 function buttonPercentTotal() {
+  if (g) {
+    x = Math.log(x);
+    afterGUnary();
+    return;
+  }
   x = (x / y) * 100;
   afterBinaryOp();
 }
 function buttonPercentChange() {
+  if (g) {
+    let wasneg = 1;
+    if (x < 0) {
+      wasneg = -1;
+    }
+
+    x = wasneg * (x * wasneg - Math.floor(x * wasneg));
+    afterGUnary();
+    return;
+  }
   x = ((x - y) / y) * 100;
   afterBinaryOp();
 }
 function buttonPercent() {
+  if (g) {
+    let wasneg = 1;
+    if (x < 0) {
+      wasneg = -1;
+    }
+    x = Math.floor(x * wasneg) * wasneg;
+
+    afterGUnary();
+    return;
+  }
   x = y * (x / 100);
   afterBinaryOp();
 }
 
 function buttonSwapXY() {
+  if (f) {
+    // clear FIN
+    N = 0;
+    I = 0;
+    PV = 0;
+    PMT = 0;
+    FV = 0;
+    f = 0;
+    return;
+  }
   let t = x;
   x = y;
   y = t;
@@ -209,11 +354,21 @@ function buttonRotateStack() {
 }
 
 function buttonRecipX() {
+  if (g) {
+    x = Math.exp(x);
+    afterGUnary();
+    return;
+  }
   x = 1 / x;
   showx();
 }
 
 function buttonYtoX() {
+  if (g) {
+    x = x ** 0.5;
+    afterGUnary();
+    return;
+  }
   x = y ** x;
   afterBinaryOp();
 }
