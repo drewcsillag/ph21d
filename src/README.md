ca43b3588316b52f5b661e0dd3cb9a02638d693e
Drew Csillag <drewc@dropbox.com>
2018-12-28 16:55:57 +0000

    updated readme, used loop to bind click handlers

--- src/README.md
+++ src/README.md
@@ -1,3 +1,9 @@
+# PH21C
+
+An implementation of a Pewlett Hackard 21 C Financial Calculator, likeness to any calculators, living or dead, is strictly coincidental.
+
+This was originally started as a way to learn webpack and modern web technologies more intimately. I use TypeScript at work, but the environment already had a number of things already dealt with, so I didn't have to know how all the stuff under the hood worked. Additionally, I usually only work ond backends, so this was a way to learn React/Redux/browser side JS/TS.
+
 # KNOWN BUGS (distinct from unimplemented)
 
 - IRR is still very wonky, and limited to -1000% to 1000%
@@ -9,3 +15,4 @@
 
 - errors are not reported correctly
 - 360 day date calculations can be off by a day
+- 365 day date calculations don't properly deal with leap years on 100/400 boundaries (or something like that)
--- src/index.tsx
+++ src/index.tsx
@@ -85,45 +85,50 @@ function showx() {
 }
 store.subscribe(showx);
 
-window.document.getElementById('buttonN').addEventListener('click', buttonN);
-window.document.getElementById('buttonI').addEventListener('click', buttonI);
-window.document.getElementById('buttonPV').addEventListener('click', buttonPV);
-window.document.getElementById('buttonPMT').addEventListener('click', buttonPMT);
-window.document.getElementById('buttonFV').addEventListener('click', buttonFV);
-window.document.getElementById('buttonCHS').addEventListener('click', buttonCHS);
-window.document.getElementById('buttonDiv').addEventListener('click', buttonDiv);
-window.document.getElementById('buttonYtoX').addEventListener('click', buttonYtoX);
-window.document.getElementById('buttonRecipX').addEventListener('click', buttonRecipX);
-window.document.getElementById('buttonPercentTotal').addEventListener('click', buttonPercentTotal);
-window.document
-  .getElementById('buttonPercentChange')
-  .addEventListener('click', buttonPercentChange);
-window.document.getElementById('buttonPercent').addEventListener('click', buttonPercent);
-window.document.getElementById('buttonEEX').addEventListener('click', buttonEEX);
-window.document.getElementById('button1').addEventListener('click', button1);
-window.document.getElementById('button2').addEventListener('click', button2);
-window.document.getElementById('button3').addEventListener('click', button3);
-window.document.getElementById('button4').addEventListener('click', button4);
-window.document.getElementById('button5').addEventListener('click', button5);
-window.document.getElementById('button6').addEventListener('click', button6);
-window.document.getElementById('button7').addEventListener('click', button7);
-window.document.getElementById('button8').addEventListener('click', button8);
-window.document.getElementById('button9').addEventListener('click', button9);
-window.document.getElementById('button0').addEventListener('click', button0);
-window.document.getElementById('buttonPlus').addEventListener('click', buttonPlus);
-window.document.getElementById('buttonPoint').addEventListener('click', buttonPoint);
-window.document.getElementById('buttonTimes').addEventListener('click', buttonTimes);
-window.document.getElementById('buttonRunStop').addEventListener('click', buttonRunStop);
-window.document.getElementById('buttonSingleStep').addEventListener('click', buttonSingleStep);
-window.document.getElementById('buttonRotateStack').addEventListener('click', buttonRotateStack);
-window.document.getElementById('buttonSwapXY').addEventListener('click', buttonSwapXY);
-window.document.getElementById('buttonCLx').addEventListener('click', buttonCLx);
-window.document.getElementById('buttonEnter').addEventListener('click', buttonEnter);
-window.document.getElementById('buttonMinus').addEventListener('click', buttonMinus);
-// window.document.getElementById('buttonOnOff').addEventListener('click', buttonOnOff);
-window.document.getElementById('buttonF').addEventListener('click', buttonF);
-window.document.getElementById('buttonG').addEventListener('click', buttonG);
-window.document.getElementById('buttonSTO').addEventListener('click', buttonSTO);
-window.document.getElementById('buttonRCL').addEventListener('click', buttonRCL);
-window.document.getElementById('buttonSigmaPlus').addEventListener('click', buttonSigmaPlus);
+const clickers: {[id: string]: () => void} = {
+  buttonN,
+  buttonI,
+  buttonPV,
+  buttonPMT,
+  buttonFV,
+  buttonCHS,
+  buttonDiv,
+  buttonYtoX,
+  buttonRecipX,
+  buttonPercentTotal,
+  buttonPercentChange,
+  buttonPercent,
+  buttonEEX,
+  button0,
+  button1,
+  button2,
+  button3,
+  button4,
+  button5,
+  button6,
+  button7,
+  button8,
+  button9,
+  buttonPlus,
+  buttonPoint,
+  buttonTimes,
+  buttonRunStop,
+  buttonSingleStep,
+  buttonRotateStack,
+  buttonSwapXY,
+  buttonCLx,
+  buttonEnter,
+  buttonMinus,
+  buttonF,
+  buttonG,
+  buttonSTO,
+  buttonRCL,
+  buttonSigmaPlus,
+  // buttonOnOff
+};
+
+Object.keys(clickers).forEach(id =>
+  window.document.getElementById(id).addEventListener('click', clickers[id])
+);
+
 showx();
