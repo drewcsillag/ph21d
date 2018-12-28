a30800dd2252300ee3fc8b69335d0d10a4d173d8
Drew Csillag <drewc@dropbox.com>
2018-12-28 00:58:28 +0000

    finally remembered to add this

---
+++ src/interfaces.d.ts
@@ -0,0 +1,60 @@
+export interface CashFlowEntry {
+  amount: number;
+  count: number;
+  flowNumber: number;
+}
+export interface State {
+  mDotDY: boolean;
+  wasG: boolean;
+  wasF: boolean;
+  hasInput: boolean;
+  wasResult: number;
+  wasSto: boolean;
+  stoOp?: string;
+
+  wasRcl: boolean;
+
+  dec: number;
+  N: number;
+  PV: number;
+  I: number;
+  FV: number;
+  PMT: number;
+  x: number;
+  y: number;
+  stack3: number;
+  stack4: number;
+  begEnd: number;
+  registers: Array<number>;
+  cashFlows: Array<CashFlowEntry>;
+}
+
+export interface StateUpdate {
+  mDotDY?: boolean;
+  wasG?: boolean;
+  wasF?: boolean;
+  hasInput?: boolean;
+  wasResult?: number;
+  wasSto?: boolean;
+  stoOp?: string;
+
+  wasRcl?: boolean;
+
+  dec?: number;
+  N?: number;
+  PV?: number;
+  I?: number;
+  FV?: number;
+  PMT?: number;
+  x?: number;
+  y?: number;
+  stack3?: number;
+  stack4?: number;
+  begEnd?: number;
+  registers?: Array<number>;
+
+  cashFlows?: Array<CashFlowEntry>;
+}
+export interface Action {
+  type: string | number;
+}
