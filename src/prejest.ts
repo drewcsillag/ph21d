8fcdac8eff0caf82943e93e44532b769470cbcbe
Drew Csillag <drew@thecsillags.com>
2019-01-08 07:44:16 -0500

    now at 13 tests (1 failing)

--- src/prejest.ts
+++ src/prejest.ts
@@ -14,6 +14,7 @@ const tests: nameFPair[] = [
       store.dispatch({type: '+'});
       const state: State = store.getState() as State;
       const x: Decimal = state.x;
+      //   const x = store.getState().x;
       if (!x.eq(7)) {
         throw 'expected 2,Enter,5,+ to be 7, but was ' + x;
       }
@@ -65,7 +66,203 @@ const tests: nameFPair[] = [
       }
     },
   ],
+  [
+    'recipXTest',
+    () => {
+      const store = createCalcStore();
+      store.dispatch({type: 2});
+      store.dispatch({type: 'recipX'});
+      const state: State = store.getState() as State;
+      const x: Decimal = state.x;
+      if (!x.eq(0.5)) {
+        throw 'expected 2,recipX to be .5 but was ' + x;
+      }
+    },
+  ],
+  [
+    'percentTest',
+    () => {
+      const store = createCalcStore();
+      store.dispatch({type: 5});
+      store.dispatch({type: 'Enter'});
+      store.dispatch({type: 2});
+      store.dispatch({type: 'percent'});
+      const state: State = store.getState() as State;
+      const x: Decimal = state.x;
+      if (!x.eq(0.1)) {
+        throw 'expected 5,Enter,2,percent to be .1 but was ' + x;
+      }
+    },
+  ],
+  [
+    'percentTotalTest',
+    () => {
+      const store = createCalcStore();
+      store.dispatch({type: 5});
+      store.dispatch({type: 'Enter'});
+      store.dispatch({type: 2});
+      store.dispatch({type: 'percentTotal'});
+      const state: State = store.getState() as State;
+      const x: Decimal = state.x;
+      if (!x.eq(40)) {
+        throw 'expected 5,Enter,2,percentTotal to be 40 but was ' + x;
+      }
+    },
+  ],
+  [
+    'percentChangeTest',
+    () => {
+      const store = createCalcStore();
+      store.dispatch({type: 5});
+      store.dispatch({type: 'Enter'});
+      store.dispatch({type: 2});
+      store.dispatch({type: 'percentChange'});
+      const state: State = store.getState() as State;
+      const x: Decimal = state.x;
+      if (!x.eq(-60)) {
+        throw 'expected 5,Enter,2,percentChange to be -60 but was ' + x;
+      }
+    },
+  ],
+  [
+    'percentChangeTest',
+    () => {
+      const store = createCalcStore();
+      store.dispatch({type: 5});
+      store.dispatch({type: 'Enter'});
+      store.dispatch({type: 2});
+      store.dispatch({type: 'ytox'});
+      const state: State = store.getState() as State;
+      const x: Decimal = state.x;
+      if (!x.eq(25)) {
+        throw 'expected 5,Enter,2,ytox to be 25 but was ' + x;
+      }
+    },
+  ],
+  [
+    'chsTest',
+    () => {
+      const store = createCalcStore();
+      store.dispatch({type: 5});
+      store.dispatch({type: 0});
+      store.dispatch({type: 'chs'});
+      let state: State = store.getState() as State;
+      let x: Decimal = state.x;
+      if (!x.eq(-50)) {
+        throw 'expected 50,CHS to be -50 but was  ' + x;
+      }
+      store.dispatch({type: 'chs'});
+      state = store.getState() as State;
+      x = state.x;
+      if (!x.eq(50)) {
+        throw 'expected 50,CHS to be -50 but was  ' + x;
+      }
+    },
+  ],
+  [
+    'computePMTTest',
+    () => {
+      const store = createCalcStore();
+      store.dispatch({type: 1});
+      store.dispatch({type: 0});
+      store.dispatch({type: 'N'});
+      store.dispatch({type: 5});
+      store.dispatch({type: 'I'});
+      store.dispatch({type: 1});
+      store.dispatch({type: 0});
+      store.dispatch({type: 0});
+      store.dispatch({type: 0});
+      store.dispatch({type: 'PV'});
+      store.dispatch({type: 5});
+      store.dispatch({type: 0});
+      store.dispatch({type: 0});
+      store.dispatch({type: 'FV'});
+      store.dispatch({type: 'PMT'});
+      const state: State = store.getState() as State;
+      const x: Decimal = state.x;
+      if (
+        !x
+          .minus(-169.2568624)
+          .abs()
+          .lessThan(0.0000001)
+      ) {
+        throw 'expected 10,N,5,I,1000,PV,500,FV,PMT to be about -169.2568624 but was ' + x;
+      }
+    },
+  ],
+  [
+    'computeFVTest',
+    () => {
+      const store = createCalcStore();
+      store.dispatch({type: 1});
+      store.dispatch({type: 0});
+      store.dispatch({type: 'N'});
+      store.dispatch({type: 5});
+      store.dispatch({type: 'I'});
+      store.dispatch({type: 1});
+      store.dispatch({type: 0});
+      store.dispatch({type: 0});
+      store.dispatch({type: 0});
+      store.dispatch({type: 'PV'});
+      store.dispatch({type: 1});
+      store.dispatch({type: 6});
+      store.dispatch({type: 9});
+      store.dispatch({type: '.'});
+      store.dispatch({type: 2});
+      store.dispatch({type: 5});
+      store.dispatch({type: 7});
+      store.dispatch({type: 'chs'});
+      store.dispatch({type: 'PMT'});
+      store.dispatch({type: 'FV'});
+      const state: State = store.getState() as State;
+      const x: Decimal = state.x;
+      if (
+        !x
+          .minus(500.0017301)
+          .abs()
+          .lessThan(0.0000001)
+      ) {
+        throw 'expected 10,N,5,I,1000,PV,-169.257,PMT,FV to be about 500.017301 but was ' + x;
+      }
+    },
+  ],
+  [
+    'computePVTest',
+    () => {
+      const store = createCalcStore();
+      store.dispatch({type: 1});
+      store.dispatch({type: 0});
+      store.dispatch({type: 'N'});
+      store.dispatch({type: 5});
+      store.dispatch({type: 'I'});
+      store.dispatch({type: 1});
+      store.dispatch({type: 6});
+      store.dispatch({type: 9});
+      store.dispatch({type: '.'});
+      store.dispatch({type: 2});
+      store.dispatch({type: 5});
+      store.dispatch({type: 7});
+      store.dispatch({type: 'chs'});
+      store.dispatch({type: 'PMT'});
+      store.dispatch({type: 5});
+      store.dispatch({type: 0});
+      store.dispatch({type: 0});
+      store.dispatch({type: 'FV'});
+      store.dispatch({type: 'PV'});
+      const state: State = store.getState() as State;
+      const x: Decimal = state.x;
+      if (
+        !x
+          .minus(1000.101445)
+          .abs()
+          .lessThan(0.0000001)
+      ) {
+        throw 'expected 10,N,5,I,500,-169.257,PMT,500,FV,PV to be about 1000.101445 but was ' + x;
+      }
+    },
+  ],
 ];
+
 export function runTests() {
   let fails: String[] = [];
   tests.forEach(test => {
@@ -75,11 +272,11 @@ export function runTests() {
       console.log(name + ' OK');
     } catch (e) {
       console.log(name + ' FAIL');
-      fails.push(name);
+      fails.push('FAIL: ' + name + ': ' + e);
     }
   });
   if (fails.length != 0) {
-    console.log('ERROR: there were failures:', fails);
+    console.log('ERROR: ' + fails.length + '/' + tests.length + ' failed:', fails);
   } else {
     console.log('SUCCESS! All tests pass!');
   }
